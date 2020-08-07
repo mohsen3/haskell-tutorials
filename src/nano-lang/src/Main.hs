d279ab3037423a426849a647be18e57fc0038c17
Mohsen Mollanoori <mohsen.mollanoori@clio.com>
2020-08-07 01:19:51 -0600

    Complete the evaluator

--- src/nano-lang/src/Main.hs
+++ src/nano-lang/src/Main.hs
@@ -4,16 +4,16 @@ import Control.Applicative
 import Control.Monad
 import ParserLib.SimpleParser
 
-parseSum :: Parser Int
-parseSum = do
-  x <- number
-  op <- char '+' <|> char '-' 
-  y <- number
-  -- guard $ x < y
-  return (if op =='+' then x + y else x - y)
+import Nano.Parser(program)
+import Nano.Evaluator(eval)
 
 main :: IO ()
 main = do
-  let r = runParser parseSum "123+111"
-  print r
+  programText <- readFile "program.nano"
+  print programText
+  let parsed = runParser program programText
+  print parsed
+  print $ case parsed of
+            Right cmds -> show $ eval cmds
+            Left err -> err
 
--- src/nano-lang/src/Nano/Evaluator.hs
+++ src/nano-lang/src/Nano/Evaluator.hs
@@ -10,23 +10,24 @@ evalWithContext :: [(FuncName, ([VarName], Exp))] -> [(VarName, Int)] -> [Comman
 evalWithContext _ _ [] = []
 evalWithContext functions variables (cmd:rest) =
     case cmd of
-        Def name params body -> undefined -- add it to the functions list and call evalWithContext recursively
-        Let name rhs -> undefined -- add it to the variables list and call evalWithContext recursively
-        Print exp -> undefined -- eval expression and add it to the return list
+        Def name params body -> evalWithContext ((name, (params, body)):functions) variables rest
+        Let name rhs -> evalWithContext functions ((name, evalExpression functions variables rhs):variables) rest
+        Print exp -> evalExpression functions variables exp : evalWithContext functions variables rest
 
 evalExpression :: [(FuncName, ([VarName], Exp))] -> [(VarName, Int)] -> Exp -> Int
 evalExpression functions variables exp =
     case exp of
         Num n -> n
-        Var name -> lookupVar variables name
+        Var name -> lookupBinding variables name
         Sum e1 e2 -> evalExpression functions variables e1 + evalExpression functions variables e2
         Mult e1 e2 -> evalExpression functions variables e1 * evalExpression functions variables e2
-        Call name params -> undefined -- find the param names and then evaluate body of the function
-                                      -- using global variable list plus the following
-                                      -- `zipWith (evalExpression functions variables) paramNames params`
+        Call name params ->
+            let (paramNames, body) = lookupBinding functions name
+                paramValues = map (evalExpression functions variables) params
+            in  evalExpression functions (zip paramNames paramValues ++ variables) body
 
 
-lookupVar :: [(VarName, Int)] -> VarName -> Int
-lookupVar [] name = error $ "Variable not found " ++ name
-lookupVar ((key, val):rest) name | key == name = val
-                                 | otherwise = lookupVar rest name
+lookupBinding :: [(String, a)] -> String -> a
+lookupBinding [] name = error $ "Variable or function not found: " ++ name
+lookupBinding ((key, val):rest) name | key == name = val
+                                     | otherwise = lookupBinding rest name
